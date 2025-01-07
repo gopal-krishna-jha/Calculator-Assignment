3288bed440866d43cbadacf0114088092b3c5fd6
gopal-krishna-jha <gopalkrishnajha06@gmail.com>
2025-01-07 11:25:13 +0530

    first commit

---
+++ Division.java
@@ -0,0 +1,13 @@
+public class Division extends Multiplication{
+
+	void Div(int num1, int num2){
+		if(num2!=0){
+		System.out.print(num1/num2);
+		}
+		else{
+		System.out.print("Denominator can't be zero");
+		}
+		
+	}
+
+}
